9aa46915c8a70888c3d5cf8d3a1057c152cee834
Felipe <54015661+FonixAU@users.noreply.github.com>
2023-09-25 15:40:24 +1000

    Update experience.js to try and allow deployment

--- pages/experience.js
+++ pages/experience.js
@@ -1,24 +1,24 @@
-import { Container, Heading, SimpleGrid, Divider, Box, Flex, ChakraProvider} from '@chakra-ui/react'
-import Section from '../components/section'
-import Layout from '../components/layouts/article'
-import StackGrid from '../components/layouts/stackgrid'
-const Experiences = () => {
-    return(
-        <Layout>
-        <Container>
-            <Heading as="h3" fontSize={20} mb={4}>
-                Experience
-            </Heading>
-            {/* <SimpleGrid columns={[1,1,2]} gap={6}>
-            <Section>
-                <StackGrid/>
-            </Section>
-            </SimpleGrid>
-            <Section>
-                <Divider/>
-            </Section> */}
-        </Container>
-        </Layout>
-    )
-}
-export default Experiences
+// import { Container, Heading, SimpleGrid, Divider, Box, Flex, ChakraProvider} from '@chakra-ui/react'
+// import Section from '../components/section'
+// import Layout from '../components/layouts/article'
+// import StackGrid from '../components/layouts/stackgrid'
+// const Experiences = () => {
+//     return(
+//         <Layout>
+//         <Container>
+//             <Heading as="h3" fontSize={20} mb={4}>
+//                 Experience
+//             </Heading>
+//             {/* <SimpleGrid columns={[1,1,2]} gap={6}>
+//             <Section>
+//                 <StackGrid/>
+//             </Section>
+//             </SimpleGrid>
+//             <Section>
+//                 <Divider/>
+//             </Section> */}
+//         </Container>
+//         </Layout>
+//     )
+// }
+// export default Experiences
