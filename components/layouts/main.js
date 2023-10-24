c15c41686a66183e9f35314fb33a358c3c0fe22d
FonixAU <fmolinariortiz@gmail.com>
2023-10-25 10:50:03 +1100

    noise start

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -3,12 +3,12 @@ import Navbar from '../navbar.js'
 import { Box, Container } from '@chakra-ui/react'
 const Main = ({ children , router}) => {
     return (
-        <Box as="main" pb ={8}>
+        <Box style={{backgroundImage: `url(/svg/noise.svg)`}} as="main" pb ={8}>
             <Head>
                 <meta name="viewport" content="width=device-width , initial-scale=1" />
                 <title>Felipe Ortiz</title>
             </Head>
-
+            
             <Navbar path={router.asPath} />
 
             <Container maxW="container.md" pt={14}>
