cc4c4595dec04b4538363503d43961664b0aa220
fmolinariortiz <fmolinariortiz@gmail.com>
2024-10-29 23:18:46 +0000

    loading

--- pages/experience.js
+++ pages/experience.js
@@ -1,5 +1,5 @@
-import React from 'react'; // Import useState from React
-import { Container, Heading, SimpleGrid, Divider, useBreakpointValue} from '@chakra-ui/react';
+import React, { Suspense } from 'react'; // Import useState from React
+import { Container, Heading, SimpleGrid, Divider, useBreakpointValue, Spinner} from '@chakra-ui/react';
 import Section from '../components/section';
 import Layout from '../components/layouts/article';
 import { ExperienceGridItem } from '../components/grid-item';
@@ -17,12 +17,13 @@ const Experiences = () => {
           Experience
         </Heading>
         <Section>
+        <Suspense fallback={<Spinner size="xl" />}>
         {isMobile ? (
         <HoneycombGrid maxHeight={'60.92px'} evenRows={3} oddRows={4} paddingVal={(3*2)*4}/>
         ) : (
         <HoneycombGrid maxHeight={'60.92px'} evenRows={5} oddRows={6} paddingVal={5*2 + 6}/>
         )}
-        
+        </Suspense>
           <Divider mt={8} />
           <SimpleGrid columns={[1, 1, 2]} gap={6} mt={4}>
             <Section>
