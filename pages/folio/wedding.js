fd4846b66816ff58eb497153c7bd65e2bd519b3a
fmolinariortiz <fmolinariortiz@gmail.com>
2025-07-28 11:12:50 +0000

    wedding done

--- pages/folio/wedding.js
+++ pages/folio/wedding.js
@@ -1,8 +1,14 @@
-import { Container, Badge, List, ListItem} from '@chakra-ui/react'
+import { Container, Badge, List, ListItem, SimpleGrid} from '@chakra-ui/react'
 // import { ExternalLinkIcon } from '@chakra-ui/icons'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
 import { Title, FolioImage, Meta} from '../../components/folioTemplate'
+import { GridItem } from '../../components/grid-item'
+import mobile from '../../public/images/folio/wedding/main.jpg'
+import gallery from '../../public/images/folio/wedding/gallery.jpg'
+import honeymoon from '../../public/images/folio/wedding/honeymoon.jpg'
+import invites from '../../public/images/folio/wedding/invite.jpg'
+import savethedate from '../../public/images/folio/wedding/savedate.jpg'
 
 const Wedding = () => {
     return (
@@ -12,21 +18,20 @@ const Wedding = () => {
                 Wedding <Badge>Live</Badge>
                 </Title>
                 <P>
-                This site is essentially my web-based resume. It has some of the projects I work on in my spare time,
-                and some things that are ok to share from previous employment.
+                This is a collection of all the parts of a wedding I thought could be digitised in an
+                effort to save costs and put a modern spin on the whole thing.
                 </P>
                 <P>
-                Mostly built from scratch, including compnents. My 2 main focuses are always UX and ease of development.
-                The hexagon pattern as an example is fully automatic. I made it so all I have to do is add an image
-                to a folder, and it will adapt itself.
+                I built a simple Vercel hosted NextJS site, but leveraged Cloudinary's robust api and
+                (most importantly) free capabilities to add a dynamic gallery to the site, so we can spice up its functionality
+                and also make sure we capture many memories without needing a whole camera crew.
                 </P>
                 <P>
-                Most of the site uses custom components I made so that I can easily customise and re-use them
-                to populate the site.
-                </P>
-                <P>
-                I&apos;ve taken up doing most of my solo work on google&apos;s IDX platform, I love the flexibility of
-                picking up where I left off from literally anywhere.
+                I then used a combination of Directus' user friendly CMS capabilities as a GUI-fied data-base that even
+                my non-techie wife could use, to manage the invites and guest list. Along with handlebars' powerful templating,
+                where I had the freedom to pair it with any css frameworks I wanted to bring the invites to the modern age.
+                The database is hosted on fly.io where the low usage keeps costs under the $5 limit, and Render for the invites which is capable
+                of spinning up in a few seconds from idle.
                 </P>
                 <List ml={4} my={4}>
                     <ListItem>
@@ -38,12 +43,19 @@ const Wedding = () => {
                     <ListItem>
                         <Meta>Stack</Meta>
                         <span>
-                            React, Bulma, Handlebars, NextJS, Directus, Python
+                            React, Bulma, Handlebars, NextJS, Directus, Python, Express
                         </span>
                     </ListItem>
                     </List>
                     <FolioImage src="\images\folio\weddingMain.png" alt="Wedding"/>
-                    <FolioImage src="\images\folio\wedding\main.png" alt="Wedding Example"/>
+                    <SimpleGrid columns={[1,2,5]} gap={6}>
+                    <GridItem id="mobile" title="Mobile"thumbnail={mobile}/>
+                    <GridItem id="gallery" title="Gallery"thumbnail={gallery}/>
+                    <GridItem id="honeymoon" title="Honeymoon"thumbnail={honeymoon}/>
+                    <GridItem id="invites" title="Invites"thumbnail={invites}/>
+                    <GridItem id="savethedate" title="Save the dates"thumbnail={savethedate}/>
+
+                    </SimpleGrid>
             </Container>
         </Layout>
     )
